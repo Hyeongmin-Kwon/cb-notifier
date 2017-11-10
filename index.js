bdb047754fbda2e55d1dccecb8a3ec6e98630a6c
kiring <hyeongmin.kwon@architectgroup.com>
2017-11-10 14:55:00 +0900

    Using afterLoad

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 let request = require('request');
 let cheerio = require('cheerio');
 let fs = require('fs');
+let afterload = require('after-load');
+
 
 let url = 'https://codebeamer.com/cb/tracker/93958';
 
+afterload(url,function(body, $){
+    let a = $('.ui-layout-center .contentArea #browseTrackerForm .contentWithMargins').html();
+    fs.writeFile('./storage/super_test.html', a, 'utf8', function(error) {
+        if (error) throw error;
+        console.log('확인용 HTML 파일 작성');
+    });
+});
+
+request(url, {timeout: 10000}, function (error, response, body) {
+    if(error) throw error;
+
+    fs.writeFile('./storage/super_test2.html', body, 'utf8', function(error) {
+        if (error) throw error;
+        console.log('확인용 HTML2 파일 작성');
+    });
+});
+
+/*
 request(url, function (error, response, body) {
     if(error) throw error;
     
     let $ = cheerio.load(body);
-    let a = $('tr.trackerItem').html();
-    console.log(a);
-    
-
-    let post_elements = $('#trackerItems');
-    post_elements.each(function() {
-        // /contentWithMargins
-        let head_file = $(this).html();
-        console.log(head_file);
-        fs.writeFile('./storage/text.html', head_file, 'utf8', function(error) {
-            if (error) throw error;
-            console.log('비동기적 파일 쓰기 완료');
-        });
+    let a = $('.ui-layout-center .contentArea #browseTrackerForm .contentWithMargins').html();
+    fs.writeFile('./storage/super_test.html', a, 'utf8', function(error) {
+        if (error) throw error;
+        console.log('확인용 HTML 파일 작성');
     });
+
 });
+*/
