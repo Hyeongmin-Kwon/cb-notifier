baf1490e8ee45462b597ddaab74706339f91802c
kiring <hyeongmin.kwon@architectgroup.com>
2017-11-10 11:33:36 +0900

    Parsing test

--- index.js
+++ index.js
@@ -2,36 +2,24 @@ let request = require('request');
 let cheerio = require('cheerio');
 let fs = require('fs');
 
-let url = 'https://www.naver.com'
+let url = 'https://codebeamer.com/cb/tracker/93958';
 
-/*
-request(url, function(error, response, body) {
-  if (error) throw error;
-    console.log(body);
-});
-
-*/
-let input = 'Test text';
-
-fs.writeFile('text.txt', input, 'utf8', function(err) {
-    console.log('비동기적 파일 쓰기 완료');
-});
-
-let text1 = fs.readFileSync('./storage/text1.txt', 'utf8');
-let text2 = fs.readFileSync('./storage/text2.txt', 'utf8');
-
-console.log(text1==text2);
-
-request(url, function(error, response, body) {
+request(url, function (error, response, body) {
     if(error) throw error;
     
     let $ = cheerio.load(body);
+    let a = $('tr.trackerItem').html();
+    console.log(a);
+    
 
-    let post_elements = $('body');
+    let post_elements = $('#trackerItems');
     post_elements.each(function() {
-        let post_title = $(this).text();
-        let post_url = $(this).find('a').attr('href');
-
-        console.log();
+        // /contentWithMargins
+        let head_file = $(this).html();
+        console.log(head_file);
+        fs.writeFile('./storage/text.html', head_file, 'utf8', function(error) {
+            if (error) throw error;
+            console.log('비동기적 파일 쓰기 완료');
+        });
     });
 });
